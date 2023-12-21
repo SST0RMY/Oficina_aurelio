8bb75b9114f9e3ab97e6d5b324b1d94e0e3eedcd
LC-Losada <a85211@ualg.pt>
2023-12-21 18:52:05 +0000

    Mudei a Feaute List

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,31 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Objetivo',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        A nossa oficina tem como objetivo ajudar os moradores da aldeia
+        sem eles precisarem se dirigir até à cidade
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'O que fazemos',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        tratamos de serviços de oficina,bateria carro,áudio, multimédia, assistentes de condução e
+        transporte
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Morada',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Oliveira do Conde, 8430-358, Viseu, Portugal
       </>
     ),
   },
